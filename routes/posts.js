710aaa925cf28e0dc943bc4935266bb0fca4ea0d
Gal Noy <gal@tikalk.com>
2016-01-03 15:40:21 +0200

    Full crud

--- routes/posts.js
+++ routes/posts.js
@@ -11,8 +11,29 @@ router.get('/', (req, res, next) => {
     });
 });
 
+router.get('/:id', (req, res, next) => {
+    db.find({_id: req.params.id}, {}, (err, docs) => {
+        if (err) throw Error(err)
+        res.json(docs)
+    });
+});
+
 router.post('/', (req, res, next) => {
     db.insert(req.body, (err, doc) => {
+        if (err) throw Error(err)
+        res.json(doc)
+    });
+});
+
+router.put('/:id', (req, res, next) => {
+    db.update({_id: req.params.id}, req.body, {}, (err, doc) => {
+        if (err) throw Error(err)
+        res.json(doc)
+    });
+});
+
+router.delete('/:id', (req, res, next) => {
+    db.remove({_id: req.params.id}, {}, (err, doc) => {
         if (err) throw Error(err)
         res.json(doc)
     });
